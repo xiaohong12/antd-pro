fd7d6754197aa3f37666743e3f135b329dc2cbb7
“henghong.jiang” <2595997982@qq.com>
2022-05-20 18:17:46 +0800

    q

--- src/app.tsx
+++ src/app.tsx
@@ -66,7 +66,7 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
     onPageChange: async () => {
       const { location } = history;
       // 如果没有登录，重定向到 login
-      if (!initialState?.currentUser && location.pathname !== loginPath) {
+      if (!initialState?.currentUser && location.pathname !==  loginPath) {
         history.push(loginPath);
       }
     },
